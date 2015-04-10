6352a5638898eba0cd5d822b31872fad66c7b2a7
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-04-10 23:09:18 +0200

    First commit.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html class="no-js"><!--<![endif]-->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Grzegorz || Bust</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="resources/css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="resume.css">
+        <link rel="shortcut icon" href="favicon.ico">
+        <link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+
+        <script src="resume.js"></script>
+
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-37385207-1']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
+    
+    </body>
+</html>
---
+++ resume.js
@@ -0,0 +1,291 @@
+var GRZEGORZPAWLIK = GRZEGORZPAWLIK || {};
+
+GRZEGORZPAWLIK.Developer = GRZEGORZPAWLIK.Developer || {};
+GRZEGORZPAWLIK.Developer.Seed = GRZEGORZPAWLIK.Developer.Seed || {};
+
+GRZEGORZPAWLIK.Developer.Seed.titles = {
+    "title": "var GRZEGORZPAWLIK = GRZEGORZPAWLIK || {};",
+    "role": "GRZEGORZPAWLIK.Developer = GRZEGORZPAWLIK.Developer || {};"
+};
+
+GRZEGORZPAWLIK.Developer.Seed.contact = {
+    "city": "Barcelona ES, 08003",
+    "email": "pawlik.gregor@gmail.com",
+    "Resume": "grzegorz-pawlik-resume-2015.pdf",
+    "LinkedIn": "http://www.linkedin.com/in/pawlikgrzegorz",
+    //"CodePen": "http://codepen.io/grzegorzpawlik/",
+    "Github": "https://github.com/gpawlik",
+    "StackOverflow": "http://stackoverflow.com/users/1359848/grzegorz-pawlik"
+    //"Coderbits": "https://coderbits.com/gpawlik"
+};
+
+GRZEGORZPAWLIK.Developer.Seed.projects = {
+    "recentProjects": {
+
+    }
+}
+
+GRZEGORZPAWLIK.Developer.Seed.tech = {
+    "HTML/CSS/JavaScript": ["Grunt", "Compass", "Sass & Less", "jQuery", "Backbone.js", "Templates: Mustache, Handlebars, Jade", "Bookmarklets", "Social media integration", "AngularJS", "Bootstrap"],
+    "PHP": ["WordPress", "CodeIgniter", "Drupal", "Joomla", "Laravel"]
+}
+
+GRZEGORZPAWLIK.Developer.Seed.refs = {
+    "professional": [{
+        "name": "Bob Samii",
+        "role": "Head of Marketing",
+        "email": "bsamii@inmotionrealestate.com "
+    }, {
+        "name": "Kim Nguyen",
+        "role": "Head of Social Media",
+        "email": "kim.nguyen@odigeo.com "
+    }, {
+        "name": "Coen Smeekes",
+        "role": "Founder",
+        "email": "coen@bluebirdmedia.nl"
+    }],
+    "personal": [{
+        "name": "Krystyna Pawlik",
+        "role": "Mom"
+    }, {
+        "name": "Tomasz Pawlik",
+        "role": "Brother"
+    }]
+};
+
+GRZEGORZPAWLIK.Developer.Seed.edu = [{
+    "school": "Wroclaw University of Technology",
+    "degree": "Master of Science",
+    "major": "Telecommunication Engineering",
+    "class": "2004"
+}, {
+    "school": "Universidad Miguel Hernández de Elche",    
+    "major": "Informatics and Telecommunications",
+    "class": "2007"
+}];
+
+GRZEGORZPAWLIK.Developer.Seed.xp = {
+    "professional": [{
+        "role": "Social Media App Developer",
+        "company": "eDreams Odigeo",
+        "description": "",
+        "date": "05/2012-now",
+        "clients": ["eDreams", "Opodo", "GoVoyages", "Travellink", "Liligo"]
+    }, {
+        "role": "Full-Stack Developer",
+        "company": "BlueBirdMedia",
+        "description": "",
+        "date": "07/2011-05/2012",
+        "clients": ["Mainly small and medium dutch-based companies"]
+    }, {
+        "role": "Full-Stack Developer",
+        "company": "Internet Advantage",
+        "description": "",
+        "date": "01/2011-08/2011",
+        "clients": ["DonQ", "Sonic Drive-In", "Ford"]
+    }, {
+        "role": "Back-End Developer",
+        "company": "Web Koncept Studio",
+        "description": "",
+        "date": "09/2008-11/2009"
+    }]
+};
+
+GRZEGORZPAWLIK.Developer.Resume = function(seed) {
+
+    if (!seed) {
+        throw ("Seed doesn't exist");
+        return;
+    }
+
+    var _seed = seed,
+        _modules = ['Title', 'Contact', 'Projects', 'Technologies',
+            'Experience', 'Education'
+        ],
+        _modulesLen = _modules.length;
+
+    var _pFuncs = {
+
+        _addTitle: function() {
+            var titles = _seed.titles,
+                str = '';
+            str += '<header class="gp-header">';
+            str += '<h1>' + titles.title + '</h1>';
+            str += '<p>' + titles.role + '</p>';
+            str += '</header>';
+
+            document.body.innerHTML += str;
+        },
+
+        _addContact: function() {
+            var contactInfo = _seed.contact,
+                item, str = '';
+
+            str += '<div class="gp-module gp-contact-info">';
+            str += this._createTitle('Contact');
+            for (item in contactInfo) {
+                str += '<div class="gp-contact">';
+                str += '<p>';
+                if (item === 'email') {
+                    str += '<a href="mailto:' + contactInfo[item] + '">';
+                    str += contactInfo[item];
+                    str += '</a>';
+                } else if (item.toLowerCase() === 'resume' || contactInfo[item].toLowerCase().indexOf('http') > -1 || item.toLowerCase() === 'http://GRZEGORZPAWLIK.com') {
+                    str += '<a href="' + contactInfo[item] + '" target="_blank">';
+                    str += item;
+                    str += '</a>';
+                } else {
+                    str += contactInfo[item]
+                }
+                str += '</p>';
+                str += '</div>';
+            }
+            str += '</div>';
+
+            document.body.innerHTML += str;
+        },
+
+        _addTechnologies: function() {
+            var techs = _seed.tech,
+                item, feature, str = '',
+                i = 0;
+
+            str += '<div class="gp-module gp-professional-technologies">';
+            str += this._createTitle('Technology');
+            for (tech in techs) {
+                str += '<h3>' + tech + '</h3>';
+                str += this._addList(techs[tech]);
+            }
+            str += '</div>';
+
+            document.body.innerHTML += str;
+        },
+
+        _addExperience: function() {
+            var xp = _seed.xp.professional,
+                xpLen = xp.length,
+                item, str = '',
+                i = 0;
+
+            str += '<div class="gp-module gp-experience">';
+            str += this._createTitle('Experience');
+            str += '<ul>';
+            for (i = 0; i < xpLen; i++) {
+                str += '<li>';
+                str += '<h3>' + xp[i].role + '</h3>';
+                str += '<p>' + xp[i].company + '</p>';
+                str += '<p>' + xp[i].description + '</p>';
+                str += '<p>' + xp[i].date + '</p>';
+                str += this._addList(xp[i].clients);
+                str += this._addList(xp[i].tools);
+                str += '</li>';
+            }
+            str += '</ul>';
+            str += '</div>';
+
+            document.body.innerHTML += str;
+        },
+
+        _addProfessionalReferences: function() {
+            var refs = _seed.refs.professional,
+                refsLen = refs.length,
+                i = 0,
+                str = '';
+
+            str += '<div class="gp-module gp-professional-references">';
+            str += this._createTitle('Professional References');
+            for (i = 0; i < refsLen; i++) {
+                str += '<p>' + refs[i].name + '</p>';
+                str += '<p>' + refs[i].role + '</p>';
+                str += '<p>' + refs[i].email + '</p>';
+                str += '<br>';
+            };
+            str += '</div>';
+
+            document.body.innerHTML += str;
+        },
+
+        _addEducation: function() {
+            var edu = _seed.edu,
+                eduLen = edu.length,
+                i = 0,
+                item, str = '';
+
+            str += '<div class="gp-module gp-education">';
+            str += this._createTitle('Education');            
+            str += '<ul>';
+            for (i = 0; i < eduLen; i++) {
+                str += '<li>';
+                for (item in edu[i]) {
+                    str += '<p>' + edu[i][item] + '</p>';
+                }
+                str += '</li>';
+            }
+            str += '</ul>';                        
+            str += '</div>';
+
+            document.body.innerHTML += str;
+        },
+
+        _addProjects: function() {
+            var recentProjects = _seed.projects.recentProjects,
+                item, str = '';
+
+            str += '<div class="gp-module gp-recentProjects">';
+            str += this._createTitle('Featured Projects');
+            str += '<ul>';
+            for (item in recentProjects) {
+                str += '<li><a href="' + recentProjects[item] + '" target="_blank">' + item + '</a></li>';
+            }
+            str += '</ul>';
+            str += '</div>';
+            document.body.innerHTML += str;
+        },
+
+        _createTitle: function(title) {
+            return '<h2>' + title + '</h2>';
+        },
+
+        _addList: function(collection) {
+            var item, str = '';
+            if (collection) collection.sort();
+            str += '<ul>';
+            for (item in collection) {
+                str += '<li>' + collection[item] + '</li>';
+            }
+            str += '</ul>';
+            return str;
+        }
+
+    };
+
+    return {
+        addAllModules: function() {
+            var i;
+            for (var i = 0; i < _modulesLen; i++) {
+                this.addModuleByName(_modules[i]);
+            };
+        },
+
+        addModuleByName: function(name) {
+            _pFuncs['_add' + name]();
+        },
+
+        destroy: function() {
+            var els = document.querySelectorAll('.gp-module'),
+                elsLen = els.length;
+            for (var i = 0; i < elsLen; i++) {
+                els[i].parentNode.removeChild(els[i]);
+            }
+        }
+    };
+};
+
+/**
+ * Entry point
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    var seed = GRZEGORZPAWLIK.Developer.Seed,
+        gp = new GRZEGORZPAWLIK.Developer.Resume(seed);
+    gp.addAllModules();
+});
